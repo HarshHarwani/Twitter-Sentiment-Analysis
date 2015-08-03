e5e7be200f2fe613b4ec257aef47cac6f064ef9e
HarshHarwani <harwaniharsh@gmail.com>
2015-08-03 15:35:30 -0400

    added comments

--- preProcessCsv.py
+++ preProcessCsv.py
@@ -3,14 +3,19 @@ import sys
 import re
 def processTweets(tweet):
 	tweet=tweet.lower()
+	#removing the url's and any web address
 	tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',tweet)
+	#removing usernames followed by @
 	tweet = re.sub('@[^\s]+','',tweet)
 	tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
 	tweet=re.sub('(?<=\w)\.(?=\w+\.)|\G\w+\K\)','',tweet)
+	#removing extra dotes
 	tweet=re.sub('[\d\.]',"",tweet)
+	#removing special characters like !,<,>,|,@
 	tweet=re.sub('[!-><|@]',"",tweet)
 	return tweet
-	
+
+#opening the csv file given as an argument and writing the cleaned tweets in a text file	
 f = open(sys.argv[1], 'rt')
 target=open("cleanedData.txt","w")
 try:
